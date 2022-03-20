a385c59fb409b44fa2a141a33e64933365c172db
Cristian Manolache <cristianmanolache0@gmail.com>
2022-03-20 18:45:13 +0200

    Initial commit

---
+++ src/main/java/com/endava/endavaproject/EndavaProjectApplication.java
@@ -0,0 +1,14 @@
+package com.endava.endavaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndavaProjectApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(EndavaProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/endava/endavaproject/EndavaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endava.endavaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndavaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
