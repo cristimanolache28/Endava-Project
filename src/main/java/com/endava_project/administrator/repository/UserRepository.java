4f73323c52dbdb9219b924fa4ebe49740adb05f0
Cristian Manolache <cristianmanolache0@gmail.com>
2022-03-21 16:05:06 +0200

    add UserRepository

---
+++ src/main/java/com/endava_project/administrator/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.endava_project.administrator.repository;
+
+import com.endava_project.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Integer> {
+}
